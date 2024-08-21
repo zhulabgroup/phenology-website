1dbda1bb23578a76303f161bb15054ad3d304148
Tanay Panja <tpanja2017@gmail.com>
2024-08-20 22:04:48 -0400

    debugged errors for other genuses

--- phenowatch/app_local_all_years_slider.R
+++ phenowatch/app_local_all_years_slider.R
@@ -57,6 +57,30 @@ today <- read_file(str_c(path_app, "/today.txt")) %>% as.Date()
 #   unlist()
 # names(genus_list)<-NULL
 
+calc_range <- function(df) {
+  non_na_indices <- which(!is.na(df$intensity))
+  flag = F
+  
+  first_instance = -1
+  last_instance = -1
+  
+  for (j in 1:(length(non_na_indices) - 1)) {
+    current_index <- non_na_indices[j]
+    next_index <- non_na_indices[j + 1]
+    
+    if (df$intensity[current_index] <= 0.25 && df$intensity[next_index] >= 0.25) {
+      if (flag == F) {
+        first_instance = current_index
+        flag = T
+      }
+    }
+    if (df$intensity[current_index] >= 0.25 && df$intensity[next_index] <= 0.25) {
+      last_instance = current_index
+    }
+  }
+  return (c(first_instance, last_instance))
+}
+
 genusoi_list <- c(
   "Acer",
   "Quercus",
@@ -87,7 +111,6 @@ fieldsAll <- c(
 )
 
 
-
 epochTime <- function() {
   as.integer(Sys.time())
 }
@@ -97,6 +120,7 @@ humanTime <- function() format(Sys.time(), "%Y%m%d-%H%M%OS")
 
 
 #####
+
 generate_output <- function(input, window = 14, radius = 100000) {
   # data_path_subset <- paste0(download_folder_path,
   #                     ifelse(input$event == "Leafing", "leaf", "flower"),
@@ -190,11 +214,14 @@ generate_output <- function(input, window = 14, radius = 100000) {
   
   # radius<-500000
   if (nrow(npn_data_all) > 1) {
+    print('input')
     print(paste(input$latitude, input$longitude))
     npn_location <- filter(npn_data_all, abs(latitude- input$latitude) <=radius/100000,
                            abs(longitude- input$longitude) <=radius/100000)
+    print('npnlocation1')
     print(npn_location)
     if (nrow(npn_location) == 0) {
+      print("yes")
       img <- jpeg::readJPEG("question.jpeg")
       p_line <- ggplot() + background_image(img) + coord_equal()
       plot_and_message <- list(
@@ -225,21 +252,24 @@ generate_output <- function(input, window = 14, radius = 100000) {
     npn_location <- npn_data_all
   }
   observations = nrow(npn_location)
+  print('observations')
   print(observations)
   year_data = list()
   past_year = as.integer(format(input$date, "%Y")) - 11
   current_year = as.integer(format(input$date, "%Y"))-1
   
   if (nrow(npn_location) > 0) {
+    print('before')
     npn_location_ts <- npn_location %>%
       dplyr::select(day_of_year, phenophase_status) %>%
       group_by(day_of_year) %>%
       summarize(intensity = mean(phenophase_status)) %>%
       ungroup() %>%
       complete(day_of_year = 1:366, fill = list(intensity = NA))
-    #print(npn_location)
+    print('after')
     min_id <- min(which(!is.na(npn_location_ts$intensity)))
     max_id <- max(which(!is.na(npn_location_ts$intensity)))
+    print('overall data')
     npn_location_ts$intensity[min_id:max_id] <-
       zoo::na.approx(
         object = npn_location_ts$intensity[min_id:max_id],
@@ -247,6 +277,7 @@ generate_output <- function(input, window = 14, radius = 100000) {
       )
     
     
+    #print('after2')
     max_id <- 0
     done <- F
     while (!done) {
@@ -264,92 +295,75 @@ generate_output <- function(input, window = 14, radius = 100000) {
     }
     
     overall_data <- npn_location_ts
-    
+
     for (i in 0:10) {
+      print(i)
       npn_location_ts <- filter(npn_location, year == past_year+i) %>%
         dplyr::select(day_of_year, phenophase_status) %>%
         group_by(day_of_year) %>%
         summarize(intensity = mean(phenophase_status)) %>%
         ungroup() %>%
         complete(day_of_year = 1:366, fill = list(intensity = NA))
-      #print(npn_location)
+
       min_id <- min(which(!is.na(npn_location_ts$intensity)))
       max_id <- max(which(!is.na(npn_location_ts$intensity)))
-      npn_location_ts$intensity[min_id:max_id] <-
-        zoo::na.approx(
-          object = npn_location_ts$intensity[min_id:max_id],
-          x = min_id:max_id, maxgap = 28
-        )
-      
-      
-      max_id <- 0
-      done <- F
-      while (!done) {
-        min_id <- min(which(!is.na(npn_location_ts$intensity[(max_id + 1):length(npn_location_ts$intensity)]))) + (max_id)
-        if (min_id == Inf) {
-          done <- T
-        } else {
-          max_id <- min(which(is.na(npn_location_ts$intensity[min_id:length(npn_location_ts$intensity)]))) - 1 + (min_id - 1)
-          if (max_id == Inf) {
-            max_id <- length(npn_location_ts$intensity)
+      if (length(unique(npn_location_ts$intensity)) == 1) {
+        year_data[[i+1]] <- data.frame(thing=c('this', 'that', 'no'))
+      } else {
+        npn_location_ts$intensity[min_id:max_id] <-
+          zoo::na.approx(
+            object = npn_location_ts$intensity[min_id:max_id],
+            x = min_id:max_id, maxgap = 28
+          )
+        print('next2')
+        max_id <- 0
+        done <- F
+        while (!done) {
+          min_id <- min(which(!is.na(npn_location_ts$intensity[(max_id + 1):length(npn_location_ts$intensity)]))) + (max_id)
+          if (min_id == Inf) {
             done <- T
+          } else {
+            max_id <- min(which(is.na(npn_location_ts$intensity[min_id:length(npn_location_ts$intensity)]))) - 1 + (min_id - 1)
+            if (max_id == Inf) {
+              max_id <- length(npn_location_ts$intensity)
+              done <- T
+            }
+            npn_location_ts$intensity[min_id:max_id] <- ptw::whit1(npn_location_ts$intensity[min_id:max_id], 10)
           }
-          npn_location_ts$intensity[min_id:max_id] <- ptw::whit1(npn_location_ts$intensity[min_id:max_id], 10)
         }
-      }
-      
-      year_data[[i+1]] <- data.frame(npn_location_ts)
-    }
-    
-    calc_range <- function(df) {
-      non_na_indices <- which(!is.na(df$intensity))
-      flag = F
-      
-      first_instance = -1
-      last_instance = -1
-      
-      for (j in 1:(length(non_na_indices) - 1)) {
-        current_index <- non_na_indices[j]
-        next_index <- non_na_indices[j + 1]
         
-        if (df$intensity[current_index] <= 0.25 && df$intensity[next_index] >= 0.25) {
-          if (flag == F) {
-            first_instance = current_index
-            flag = T
-          }
-        }
-        if (df$intensity[current_index] >= 0.25 && df$intensity[next_index] <= 0.25) {
-          last_instance = current_index
-        }
+        year_data[[i+1]] <- data.frame(npn_location_ts)
       }
-      return (c(first_instance, last_instance))
     }
-    
-    if (nrow(npn_location) > 100) {
+    if (TRUE) {
       rect_data <- data.frame(
         xmin = numeric(),
         xmax = numeric(),
         ymin = numeric(),
         ymax = numeric()
       )
+      print('after7')
       for (i in 0:10) {
-        values <- calc_range(year_data[[i+1]])
-        
-        first_instance <- values[1]
-        last_instance <- values[2]
-        
-        if (first_instance != -1 && last_instance != -1) {
-          rect_data <- rbind(rect_data, data.frame(
-            xmin = past_year + i - 0.3,
-            xmax = past_year + i + 0.3,
-            ymin = first_instance,
-            ymax = last_instance
-          ))
+        if (length(which(!is.na(year_data[[i+1]]$intensity))) > 4) {
+          print('works')
+          values <- calc_range(year_data[[i+1]])
+          first_instance <- values[1]
+          last_instance <- values[2]
+          print(first_instance)
+          print(last_instance)
+          if (first_instance != -1 && last_instance != -1) {
+            rect_data <- rbind(rect_data, data.frame(
+              xmin = past_year + i - 0.3,
+              xmax = past_year + i + 0.3,
+              ymin = first_instance,
+              ymax = last_instance
+            ))
+          }
+        } else {
+          message = "Years not represented on the figure have insufficient data"
         }
       }
-      
       ylim_max <- if (nrow(rect_data) > 0) max(rect_data$ymax) + 25 else 366
-      
       rect_graph <- ggplot() +
         geom_rect(data = rect_data, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), color = "black", fill = "white", size = 0.7) +
         scale_x_continuous(breaks = seq(past_year, current_year), limits = c(past_year - 0.3, current_year + 0.3)) +
@@ -377,6 +391,7 @@ generate_output <- function(input, window = 14, radius = 100000) {
           fill = "count"
         ) +
         theme_classic()
+      print("npnlocation")
       print(npn_location)
       overall_data$intensity <- overall_data$intensity * 100
       p_line <- ggplot() +
@@ -390,7 +405,8 @@ generate_output <- function(input, window = 14, radius = 100000) {
                   stat = "bin2d", bins = c(366, 20), alpha = 1) +
         scale_fill_gradient(low = "#FFF700", high = "#F4C430", name = "Frequency of no") +
         geom_line(data = overall_data, aes(x = day_of_year, y = intensity), col = "blue", lwd = 2) +
-        geom_point(aes(x = as.integer(format(input$date, "%j")), y = as.integer(input$status == "Yes") * 100), col = "red", cex = 5) +
+        geom_point(aes(x = as.integer(format(input$date, "%j")), y = as.integer(input$status == "Yes") * 100), col = "#ff0000", cex = 5) +
+        geom_vline(xintercept=as.integer(format(input$date, "%j")), col="#ff0000", lwd=1.5, linetype="dashed") + 
         ylim(-10, 110) +
         labs(
           x = "Day of Year",
@@ -942,7 +958,7 @@ generate_output <- function(input, window = 14, radius = 100000) {
   #                                      message_attribute))
   plot_and_message <- list(
     plot = list(p_line, p_map, c_line, rect_graph),
-    message = list("")
+    message = 'Years not represented on the figure have insufficient data'
   )
 
   return(plot_and_message)
@@ -953,7 +969,7 @@ generate_plot <- function(plot_and_message, input) {
     input$plot == "Line" ~ 1,
     input$plot == "Map" ~ 2 ,
     input$plot == "Trends Between Years" ~ 3,
-    input$plot == "Threshold Plot" ~ 4
+    input$plot == "Boxplot" ~ 4
     # input$plot=="Function"~3
   )]]
 
@@ -1068,7 +1084,7 @@ shinyApp(
             selectInput(
               "plot", "Plot",
               c(
-                "Line", "Map", "Trends Between Years", "Threshold Plot"
+                "Line", "Map", "Threshold Plot"
                 # , "Function"
               )
             )
@@ -1234,3 +1250,4 @@ shinyApp(
     })
   }
 )
+
