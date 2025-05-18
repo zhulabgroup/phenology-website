763cc9af99d7dc9c4c8dec91de841e31eaf54470
yiluansong <yiluansong@gmail.com>
2025-05-18 09:36:39 -0400

    test

--- app.R
+++ app.R
@@ -1,2 +1,8 @@
 library(shiny)
-shinyApp(ui = fluidPage("OK"), server = function(input, output) {})
+shinyApp(ui = fluidPage("OK"), 
+         server = function(input, output) {},
+         options = list(
+           host = "0.0.0.0",
+           port = as.numeric(Sys.getenv("PORT", unset = 3838))
+         )
+         )
